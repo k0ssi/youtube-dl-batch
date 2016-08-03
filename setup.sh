d651f9b4d53c651e7723343831261d3cb70d000e
Steven Kosyra <steven.kosyra@googlemail.com>
2016-08-03 11:48:41 +0200

    requirements updated

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # installing requirements
 sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
-sudo apt-get install screen libav-tools git vsftpd -y 
+sudo apt-get install screen htop libav-tools git vsftpd -y 
 sudo useradd -m -d /opt dl
 echo dl:dl123 | chpasswd
+git clone https://github.com/k0ssi/youtube-dl-batch.git
